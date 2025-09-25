89e5fe5a85b642d0e66a14288db80ac07e170c34
BouillonArnaud <la239012@student.helha.be>
2025-09-25 12:15:42 +0200

    verification mask

--- subnet.py
+++ subnet.py
@@ -1,6 +1,40 @@
 import ipaddress
 import math
 
+def is_masque_valide(masque):
+    try:
+        # Découpe du masque en octets
+        octets = masque.split('.')
+        if len(octets) != 4:
+            return False
+
+        #Vérification que chaque octet est entre 0 et 255
+        valeurs = []
+
+        for octet in octets:
+            if not octet.isdigit():
+                return False
+            v = int(octet)
+            if v < 0 or v > 255:
+                return False
+            valeurs.append(v)
+
+        #Conversion du masque en binaire
+        binaire = "".join(f"{v:08b}" for v in valeurs)
+
+        #Vérifier que le masque est un suite de 1 suivie d'une suite de 0
+        if "01" in binaire:
+            i = binaire.index("01")
+            # Après le 01, plus de 1
+            if "1" in binaire[i + 2]:
+                return False
+        # On exclu les masques 0.0.0.0 et 255.255.255.255
+        if not ("1" in binaire and "0" in binaire):
+            return False
+
+        return True
+    except Exception:
+        return False
 
 def masque_pointee_to_cidr(masque_pointee):
     """
@@ -9,11 +43,15 @@ def masque_pointee_to_cidr(masque_pointee):
     :param masque_pointee: Masque réseau en notation pointée (ex: 255.255.255.0)
     :return: Notation CIDR (ex: 24)
     """
-    # Convertir le masque pointée en binaire
-    binary_mask = ''.join([bin(int(x))[2:].zfill(8) for x in masque_pointee.split('.')])
 
-    # Compter le nombre de 1 dans le masque binaire
-    return binary_mask.count('1')
+    if is_masque_valide(masque_pointee):
+        # Convertir le masque pointée en binaire
+        binary_mask = ''.join([bin(int(x))[2:].zfill(8) for x in masque_pointee.split('.')])
+
+        # Compter le nombre de 1 dans le masque binaire
+        return binary_mask.count('1')
+    else:
+        raise ValueError("Masque invalide")
 
 def decoupage_classique(adresse_ip, masque_pointee, nombre_sr):
     """
